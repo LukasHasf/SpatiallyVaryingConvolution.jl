1699ba985861f404d3bd3de2b10245e399829f37
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-05-10 13:58:12 +0200

    Move some functions to utils

--- src/SpatiallyVaryingConvolution.jl
+++ src/SpatiallyVaryingConvolution.jl
@@ -1,40 +1,13 @@
 module SpatiallyVaryingConvolution
 
-using MAT, HDF5
+include("utils.jl")
 using NDTools
 using FourierTools
 using FFTW
 using Arpack, LinearAlgebra
 using ScatteredInterpolation
 
-"""    
-    readPSFs(path::String, key::String)
-
-Read the PSFs stored in file `path` accessible as field `key`. 
-
-Supports MAT and HDF5 file format. 
-PSFs are expected to be stored as an array at `key` having shape
- (Ny, Nx, nrPSFs) in the 2D case or (Ny, Nx, nrPSFs, Nz) in the 3D case.
- # Examples
- ```
- julia> readPSFs("myPSFs.mat", "PSFs")
- ```
- ```
- julia> readPSFs("myPSFs.h5", "myPSFsDataset")
- ```
-"""
-function readPSFs(path::String, key::String)
-    if occursin(".mat", path)
-        file = matopen(path)
-    elseif occursin(".h5", path)
-        file = h5open(path, "r")
-    end
-    if haskey(file, key)
-        psfs = read(file, key)
-        return psfs
-    end
-end
-
+export generate_model, readPSFs
 """
     registerPSFs(stack, ref_im)
 
@@ -149,14 +122,6 @@ function interpolate_weights(weights, shape, si)
         weights_interp[:, :, r] .= reshape(evaluate(itp, gridPoints), (Nx, Ny))'
     end
     return weights_interp
-end
-
-function pad2D(x)
-    return select_region(x, new_size=2 .* size(x)[1:2], pad_value=zero(eltype(x)))
-end
-
-function crop2D(x, rcL, rcU, ccL, ccU)
-    return x[rcL:rcU, ccL:ccU]
 end
 
 """
---
+++ src/utils.jl
@@ -0,0 +1,37 @@
+using MAT, HDF5
+export readPSFs, pad2D, crop2D
+"""    
+    readPSFs(path::String, key::String)
+
+Read the PSFs stored in file `path` accessible as field `key`. 
+
+Supports MAT and HDF5 file format. 
+PSFs are expected to be stored as an array at `key` having shape
+ (Ny, Nx, nrPSFs) in the 2D case or (Ny, Nx, nrPSFs, Nz) in the 3D case.
+ # Examples
+ ```
+ julia> readPSFs("myPSFs.mat", "PSFs")
+ ```
+ ```
+ julia> readPSFs("myPSFs.h5", "myPSFsDataset")
+ ```
+"""
+function readPSFs(path::String, key::String)
+    if occursin(".mat", path)
+        file = matopen(path)
+    elseif occursin(".h5", path)
+        file = h5open(path, "r")
+    end
+    if haskey(file, key)
+        psfs = read(file, key)
+        return psfs
+    end
+end
+
+function pad2D(x)
+    return select_region(x, new_size=2 .* size(x)[1:2], pad_value=zero(eltype(x)))
+end
+
+function crop2D(x, rcL, rcU, ccL, ccU)
+    return x[rcL:rcU, ccL:ccU]
+end
