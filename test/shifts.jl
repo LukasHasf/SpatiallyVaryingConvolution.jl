0cee1c40489efeed908f942ae26f6dab098fb868
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-06-07 10:18:18 +0200

    Add test

--- test/shifts.jl
+++ test/shifts.jl
@@ -12,6 +12,9 @@
         SpatiallyVaryingConvolution.registerPSFs(psfs[:, :, :], psfs[:, :, nrPSFs÷2+1])
         return shifts
     end
+    lower_index(N) = Bool(N % 2) ? (N+3)÷2 : (N+2)÷2
+    upper_index(N) = Bool(N % 2) ?  3*N÷2 +1 : 3*N÷2
+
     @testset "Test shift is zero for exact same PSF" begin
         in_shifts = zeros(Int32, (2, 9))
         @test in_shifts ≈ -calc_shifts(in_shifts)
@@ -21,5 +24,20 @@
         in_shifts = rand(-200:200, (2, 17))
         in_shifts[:, size(in_shifts)[2]÷2+1].= 0
         @test in_shifts ≈ -calc_shifts(in_shifts)
+    end
+
+    @testset "Test registration works for more complex shift invariant PSFs" begin
+        nrPSFs = 17
+        in_shifts = rand(-200:200, (2, nrPSFs))
+        in_shifts[:, size(in_shifts)[2]÷2+1].= 0
+        Ny, Nx = 400, 400
+        padded_psf = pad2D(rand(Float64, Ny, Nx))
+        psfs = Array{Float64, 3}(undef, Ny, Nx, nrPSFs)
+        for i in 1:nrPSFs
+            psfs[:, :, i] .= circshift(padded_psf, in_shifts[:, i])[lower_index(Ny): upper_index(Ny), lower_index(Nx): upper_index(Nx)]
+        end
+        psfs_reg, shifts =
+        SpatiallyVaryingConvolution.registerPSFs(psfs, psfs[:, :, nrPSFs÷2+1])
+        @test in_shifts ≈ -shifts
     end
 end
